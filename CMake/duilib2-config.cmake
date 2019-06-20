c508cb2ee46dad6fab00178781be2969ce964627
winsoft666 <winsoft666@outlook.com>
2019-06-20 15:55:31 +0800

    update cmake.

--- CMake/duilib2-config.cmake
+++ CMake/duilib2-config.cmake
@@ -2,10 +2,10 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-if(EXISTS ${SELF_DIR}/duilib-target.cmake)
-	include(${SELF_DIR}/duilib-target.cmake)
+if(EXISTS ${SELF_DIR}/duilib2-target.cmake)
+	include(${SELF_DIR}/duilib2-target.cmake)
 endif()
 
-if(EXISTS ${SELF_DIR}/duilib-static-target.cmake)
-	include(${SELF_DIR}/duilib-static-target.cmake)
+if(EXISTS ${SELF_DIR}/duilib2-static-target.cmake)
+	include(${SELF_DIR}/duilib2-static-target.cmake)
 endif()
